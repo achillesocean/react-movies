da64f3cc966c9a5237c57e7cc608c48af9ea62cd
Felmeta Dereje <0202sent@gmail.com>
2025-01-25 00:31:02 +0300

    final touches

--- src/appwrite.js
+++ src/appwrite.js
@@ -33,3 +33,16 @@ export const updateSearchCount = async (searchTerm, movie) => {
     }
   } catch (error) {}
 };
+
+export const getTrendingMovies = async () => {
+  try {
+    const result = await database.listDocuments(DATABASE_ID, TABLE_ID, [
+      Query.limit(5),
+      Query.orderDesc("count"),
+    ]);
+
+    return result.documents;
+  } catch (error) {
+    console.error(`Error when fetching trending movies: ${error}`);
+  }
+};
