3da3dfcca3cf2d2ec2f1648efcc162b9f7fad197
Felmeta Dereje <0202sent@gmail.com>
2025-01-25 18:23:31 +0300

    search func

--- src/App.jsx
+++ src/App.jsx
@@ -22,12 +22,14 @@ function App() {
   const [movieList, setMovieList] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
 
-  const fetchMovies = async () => {
+  const fetchMovies = async (query = "") => {
     setIsLoading(true);
     setErrorMessage("");
 
     try {
-      const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
+      const endpoint = query
+        ? `${API_BASE_URL}/search/movie?query=${encodeURIComponent(query)}`
+        : `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
 
       const response = await fetch(endpoint, API_OPTIONS);
 
@@ -53,7 +55,7 @@ function App() {
   };
 
   useEffect(() => {
-    fetchMovies();
+    fetchMovies(searchTerm);
   }, [searchTerm]);
 
   return (
